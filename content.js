4b77847f682b5ff8aac3d5b7b8510ceb814dfa55
j835111 <j835111@gmail.com>
2026-05-02 17:22:35 +0800

    Add fallback video selector for YouTube speed control

--- content.js
+++ content.js
@@ -4,7 +4,7 @@
   const MAX_RATE = 16;
 
   function getVideo() {
-    return document.querySelector('.html5-main-video');
+    return document.querySelector('.html5-main-video') || document.querySelector('video');
   }
 
   function clamp(value, min, max) {
